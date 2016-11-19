11848ddf51c55ad88c64b584887d4b77a88a0a45
davis <davis.kuriath@gmail.com>
2016-11-19 15:03:51 -0500

    playbook executor file tuned for working with Apache

--- play_util/AnsiblePlaybook.py
+++ play_util/AnsiblePlaybook.py
@@ -1,4 +1,4 @@
-#!/home/davis/Documents/netautgui/flask/bin/python2.7
+#!/var/www/html/ansibengineapi/flask/bin/python2.7
 import os
 import sys
 file_path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.path.pardir))
@@ -17,9 +17,9 @@ from ansible.executor.playbook_executor import PlaybookExecutor
 class AnsiblePlaybook:
 
   def __init__(self,playbook_path,host_list,stdoutfile):
-      self.playbook_path='/home/davis/Documents/Network-automation/'+str(playbook_path).strip()
+      self.playbook_path='/var/ansible/Network-automation/'+str(playbook_path).strip()
 
-      self.host_list='/home/davis/Documents/Network-automation/'+str(host_list).strip()
+      self.host_list='/var/ansible/Network-automation/'+str(host_list).strip()
 
       self.stdoutfile = stdoutfile
 
@@ -37,8 +37,9 @@ class AnsiblePlaybook:
         sys.exit()
 
     Options = namedtuple('Options', ['listtags', 'listtasks', 'listhosts', 'syntax', 'connection','module_path', 'forks', 'remote_user', 'private_key_file', 'ssh_common_args', 'ssh_extra_args', 'sftp_extra_args', 'scp_extra_args', 'become', 'become_method', 'become_user', 'verbosity', 'check'])
-    options = Options(listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh', module_path=None, forks=100, remote_user='slotlocker', private_key_file=None, ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=True, become_method=None, become_user='root', verbosity=None, check=False)
+    options = Options(listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh', module_path=None, forks=100, remote_user='slotlocker', private_key_file=None, ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=True, become_method='sudo', become_user='root', verbosity=None, check=False)
 
+    variable_manager.extra_vars = {'ansible_user': 'davis', 'ansible_password': 'fallback'} # Here are the variables used in the winclones.yml
     #variable_manager.extra_vars = {'hosts': 'mywebserver'} # This can accomodate various other command line arguments.`
 
     passwords = {}
